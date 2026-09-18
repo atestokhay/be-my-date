00dd7ebb3eb15e9db5e55162f4e8d21b175fb94f
Ates Tokhay <tokhays@gmail.com>
2026-09-18 16:34:21 +0300

    script.js

--- script.js
+++ script.js
@@ -93,6 +93,6 @@ confirmBtn.addEventListener("click", () => {
     const encodedSubject = encodeURIComponent(subject);
     const encodedMessage = encodeURIComponent(message);
     
-    const whatsappLink = `https://wa.me/${905372548545}?text=${encodedSubject}&body=${encodedMessage}`;
+    const whatsappLink = `https://wa.me/905372548545?text=${encodedSubject}%0A%0A${encodedMessage}`;
     window.location.href = whatsappLink;
 });
